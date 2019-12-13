6d6aa65fe229817d4b7076cb5adaecbacc7223f0
Murilo Martins <murilo.mtds@gmail.com>
2019-12-13 11:36:48 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/murilomartins/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/murilomartins/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.murilomartins.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern="dd/mm/yyyy")
 	private Date davaVencimento;
+	
+	@JsonFormat(pattern="dd/mm/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
