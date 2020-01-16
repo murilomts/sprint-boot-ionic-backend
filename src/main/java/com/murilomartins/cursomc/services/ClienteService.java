2379b3590f10ff3a69dd61af8fcf3d5324cda680
Murilo Martins <murilo.mtds@gmail.com>
2020-01-16 17:00:15 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/murilomartins/cursomc/services/ClienteService.java
+++ src/main/java/com/murilomartins/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		repo.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos .");
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos.");
 		}
 	}
 	
